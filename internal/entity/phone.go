c5ebe7bd09882629867733350deb93f8c7dc3e66
Kirill Kurochkin <dev.kurochkin@yandex.ru>
2024-04-20 21:37:07 +0300

    Remove builder pattern from phone & added phone repository layer

--- internal/entity/phone.go
+++ internal/entity/phone.go
@@ -1,15 +1,7 @@
 package entity
 
-type PhoneBuilder interface {
-	Brand(brand string) PhoneBuilder
-	Model(model string) PhoneBuilder
-	Year(year int) PhoneBuilder
-	OS(os string) PhoneBuilder
-	Processor(processor string) PhoneBuilder
-	Build() Phone
-}
-
 type Phone struct {
+	Id        int
 	Brand     string
 	Model     string
 	Year      int
@@ -17,35 +9,10 @@ type Phone struct {
 	Processor string
 }
 
-type ConcretePhoneBuilder struct {
-	phone Phone
-}
-
-func (pb *ConcretePhoneBuilder) Brand(brand string) PhoneBuilder {
-	pb.phone.Brand = brand
-	return pb
-}
-
-func (pb *ConcretePhoneBuilder) Model(model string) PhoneBuilder {
-	pb.phone.Model = model
-	return pb
-}
-
-func (pb *ConcretePhoneBuilder) Year(year int) PhoneBuilder {
-	pb.phone.Year = year
-	return pb
-}
-
-func (pb *ConcretePhoneBuilder) OS(os string) PhoneBuilder {
-	pb.phone.OS = os
-	return pb
-}
-
-func (pb *ConcretePhoneBuilder) Processor(processor string) PhoneBuilder {
-	pb.phone.Processor = processor
-	return pb
-}
-
-func (pb *ConcretePhoneBuilder) Build() Phone {
-	return pb.phone
+type PhoneInputDto struct {
+	Brand     string
+	Model     string
+	Year      int
+	OS        string
+	Processor string
 }
--- internal/repository/psql/phone.go
+++ internal/repository/psql/phone.go
@@ -1,1 +1,63 @@
 package psql
+
+import (
+	"context"
+	"crud-go/internal/entity"
+	"database/sql"
+)
+
+type Phones struct {
+	db *sql.DB
+}
+
+func NewBooks(db *sql.DB) *Phones {
+	return &Phones{db: db}
+}
+
+func (p *Phones) GetPhoneById(ctx context.Context, id int) (entity.Phone, error) {
+	var ph entity.Phone
+	err := p.db.QueryRow("SELECT * FROM phones WHERE id = $1", id).
+		Scan(&ph.Id, &ph.Brand, &ph.Model, &ph.Year, &ph.OS, &ph.Processor)
+	if err != nil {
+		return ph, err
+	}
+
+	return ph, nil
+}
+
+func (p *Phones) GetAllPhones(ctx context.Context) ([]entity.Phone, error) {
+	var phones []entity.Phone
+
+	rows, err := p.db.Query("SELECT * FROM phones")
+	if err != nil {
+		return phones, err
+	}
+
+	for rows.Next() {
+		var ph entity.Phone
+		err := rows.Scan(&ph.Id, &ph.Brand, &ph.Model, &ph.Year, &ph.OS, &ph.Processor)
+		if err != nil {
+			return phones, err
+		}
+		phones = append(phones, ph)
+	}
+
+	return phones, nil
+}
+
+func (p *Phones) CreatePhone(ctx context.Context, ph entity.PhoneInputDto) error {
+	_, err := p.db.Exec("INSERT INTO phones (brand, model, year, os, processor) VALUES ($1, $2, $3, $4, $5)",
+		ph.Brand, ph.Model, ph.Year, ph.OS, ph.Processor)
+	return err
+}
+
+func (p *Phones) UpdatePhoneById(ctx context.Context, id int, ph entity.PhoneInputDto) error {
+	_, err := p.db.Exec("UPDATE books SET brand=$1, model=$2, year=$3, os=$4, processor=$5 WHERE id=$6",
+		ph.Brand, ph.Model, ph.Year, ph.OS, ph.Processor, id)
+	return err
+}
+
+func (p *Phones) DeletePhoneById(ctx context.Context, id int) error {
+	_, err := p.db.Exec("DELETE FROM books WHERE id = $1", id)
+	return err
+}
